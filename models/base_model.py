975ed3c9c4ee14f962f9c3d50d36a2f84d380e4a
Samia8Asim <samia2003.8@gmail.com>
2024-02-11 14:16:42 +0200

    3

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,7 @@ import json
 class BaseModel:
     """BaseModel of the project"""
     def __init__(self, *args, **kwargs):
+        """__init__ method of class"""
         from models import storage
         if kwargs:
             for key, value in kwargs.items():
@@ -28,15 +29,18 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
+        """should print the following"""
         return "[{}] ({}) {}".format(
                 self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """updates the public instance attribute"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """dictionary containing all keys/values """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
